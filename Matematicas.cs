8b8566170ab33b3c3bceb63b40c8229d68d4f0d3
MiguelAngelFernandez <migfergom3@alu.edu.gva.es>
2025-11-24 09:30:50 +0100

    Máximo Común Divisor

--- Matematicas.cs
+++ Matematicas.cs
@@ -7,4 +7,17 @@ public class Matematicas
         if (n <= 1) return n;
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
+
+
+public static int MaximoComunDivisor(int a, int b)
+  {
+      while (b != 0)
+      {
+          int temp = b;
+          b = a % b;
+          a = temp;
+      }
+      return a;
+  }
+
 }
